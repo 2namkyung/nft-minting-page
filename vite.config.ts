fe4af823d37efd2520df2ca32a0f76cc4554dcf5
2namkyung <iddbxk@daum.net>
2022-07-15 14:57:14 +0900

    vite.config 파일 정리

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  define: {
-    global: {},
-  },
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
 });
