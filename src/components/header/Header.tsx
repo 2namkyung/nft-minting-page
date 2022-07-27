480e737307ef1e83ec9268497d84f9d5f4b2d7cc
2namkyung <iddbxk@daum.net>
2022-07-27 18:50:17 +0900

    navbar 색깔 변경

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import RainbowWallet from './RainbowWallet';
 
 function Header() {
   const navigate = useNavigate();
 
+  const location = useLocation();
+
   return (
     <>
       <div className="lg:flex justify-between mb-2">
-        <div className="flex items-center p-2 text-xl font-bold text-violet-600">
-          <div className="mr-2" onClick={() => navigate('/')}>
+        <div className="flex items-center p-2 text-xl font-bold">
+          <div
+            className={`mr-2 ${
+              location.pathname === '/' ? 'text-violet-600' : 'text-white'
+            } `}
+            onClick={() => navigate('/')}
+          >
             PUBLIC
           </div>
-          <div onClick={() => navigate('/private')}>PRIVATE</div>
+          <div
+            className={`mr-2 ${
+              location.pathname === '/private'
+                ? 'text-violet-600'
+                : 'text-white'
+            }`}
+            onClick={() => navigate('/private')}
+          >
+            PRIVATE
+          </div>
         </div>
         <div className="p-2">
           <RainbowWallet />
